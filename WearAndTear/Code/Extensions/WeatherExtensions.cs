86e995d6833cd08ed47eb020e5caf28442d08442
_Kristof_ <krizziewissie@gmail.com>
2025-02-16 21:10:38 +0100

    Attempt to fix NaN issues

--- WearAndTear/Code/Extensions/WeatherExtensions.cs
+++ WearAndTear/Code/Extensions/WeatherExtensions.cs
@@ -8,7 +8,7 @@ namespace WearAndTear.Code.Extensions
         public static ClimateCondition GetPastAverageClimateCondition(this IWorldAccessor world, BlockPos pos, double timePassed, double pollInterval)
         {
             var now = world.Calendar.TotalDays;
-            if (timePassed == 0) return world.BlockAccessor.GetClimateAt(pos, EnumGetClimateMode.ForSuppliedDate_TemperatureRainfallOnly, now);
+            if (timePassed <= 0) return world.BlockAccessor.GetClimateAt(pos, EnumGetClimateMode.ForSuppliedDate_TemperatureRainfallOnly, now);
             var pollCount = 0;
             var totalRainfall = 0f;
             var totalTemperature = 0f;
@@ -17,11 +17,29 @@ namespace WearAndTear.Code.Extensions
             {
                 var poll = world.BlockAccessor.GetClimateAt(pos, EnumGetClimateMode.ForSuppliedDate_TemperatureRainfallOnly, now - timePassed);
 
-                totalRainfall += poll.Rainfall;
-                totalTemperature += poll.Temperature;
-
-                pollCount++;
+                if(!float.IsNaN(poll.Rainfall) && !float.IsNaN(poll.Temperature))
+                {
+                    totalRainfall += poll.Rainfall;
+                    totalTemperature += poll.Temperature;
+                    pollCount++;
+                }
+                else if(WearAndTearModSystem.Config.EnableDebugLogging)
+                {
+                    world.Logger.Warning($"WearAndTear: Invalid climate data at {pos} for totalDays {now - timePassed} (skipping poll)");
+                }
+                
                 timePassed -= pollInterval;
+            }
+
+            if (pollCount == 0)
+            {
+                if(WearAndTearModSystem.Config.EnableDebugLogging) world.Logger.Warning("WearAndTear: totally failed to poll climate data (returning default template)");
+
+                return new ClimateCondition
+                {
+                    Rainfall = 0.5f,
+                    Temperature = 15f,
+                };
             }
 
             return new ClimateCondition
